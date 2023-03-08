c4d71f4bb7039b9298551d0bfb50c960f77a99a0
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2023-03-08 15:19:22 +0700

    Edit views.py

--- model_app/views.py
+++ model_app/views.py
@@ -3,22 +3,20 @@ from .controllers import pred
 
 def calculate(request):
     if request.method == 'POST':
-        income = request.POST['name']
+        income = int(request.POST['income'])
         num_family = int(request.POST['num_family'])
         num_kids = int(request.POST['num_kids'])
-        
-        pred_res = pred([income, num_family, num_kids])
 
-        
+        pred_res = pred([income/1000, num_family, num_kids], income/1000)
+
         calculations = [
-            f"Hello!, here's your budget recommendation",
-            f"{'Kesehatan'}: Rp{pred_res[0]}",
-            f"{'Pendidikan'}: Rp{pred_res[1]}",
-            f"{'Pangan'}: Rp{pred_res[2]}",
-            f"{'Tabungan'}: Rp{pred_res[3]}",
-            f"{'Keinginan'}: Rp{pred_res[4]}",
-            f"{'Operational'}: Rp{pred_res[5]}",
-            f"{'Fixed Expenses'}: Rp{pred_res[6]}",
+            f"{'Kesehatan'}: Rp {pred_res['Kesehatan']}",
+            f"{'Pendidikan'}: Rp {pred_res['Pendidikan']}",
+            f"{'Pangan'}: Rp {pred_res['Pangan']}",
+            f"{'Tabungan'}: Rp {pred_res['Saving']}",
+            f"{'Keinginan'}: Rp {pred_res['Wants']}",
+            f"{'Operational'}: Rp {pred_res['Operational']}",
+            f"{'Fixed Expenses'}: Rp {pred_res['Fixed']}",
         ]
         return render(request, 'home.html', {'calculations': calculations})
     return render(request, 'home.html')
