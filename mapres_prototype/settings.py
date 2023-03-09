d23799303262813d9215b97336539e085e721280
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2023-03-09 14:54:06 +0700

    Config Vercel again

--- mapres_prototype/settings.py
+++ mapres_prototype/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ghf-us_*fak+n$jy5rsjin4ln=8ervsru#qzgx4a%5wzimsx!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'mapres_prototype.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
